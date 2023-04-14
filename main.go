fc50ecf3f3107d4678ad73cd5022f17d45cae669
charles <muchpaul2@gmail.com>
2023-04-14 22:21:37 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.POST("/upload", uploadImages)
+	router.Run(":8080")
+}
+
+func uploadImages(c *gin.Context) {
+	fmt.Print("hello. apk knoked")
+	// Get the file from the request
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create the assets folder if it doesn't exist
+	assetsDir := "assets"
+	if _, err := os.Stat(assetsDir); os.IsNotExist(err) {
+		if err := os.Mkdir(assetsDir, 0755); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+	}
+	// Save the file to the assets folder
+	filename := filepath.Base(file.Filename)
+	dst := fmt.Sprintf("%s/%s", assetsDir, filename)
+	if err := c.SaveUploadedFile(file, dst); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("File '%s' uploaded successfully.", filename)})
+}
